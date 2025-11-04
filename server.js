d82118b399cd77c2cafdd6a918aba9bd7670eb89
Bahanack-GY <georgesyvan12@gmail.com>
2025-11-04 06:21:56 +0100

    Learned hos to use .env file

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+console.log('MongoDB URI: ', process.env.MONGODB_URI);
+
+app.get('/', (req,res) =>{
+    res.send("Hello world")
+})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
